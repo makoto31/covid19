01365264760504a1367c4f3c852aa8675e503e62
mac <>
2020-03-28 00:35:22 +0900

    html叩き台

--- src/csv_parse/main.ts
+++ src/csv_parse/main.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import { join } from 'path';
 
 async function main(){
-    // 県別 日付ごと
+    // 日付ごと
     const data:{[key:string]:{[key:string]:number}} = {};
 
     const csv = await fs.readFile(join(__dirname, 'individuals.csv'), 'utf8');
@@ -17,21 +17,22 @@ async function main(){
 
         const prefecture = items[7];
         // 国内のみ
-        if(prefecture.includes('中国')){
+        if(prefecture.includes('中国') || prefecture.includes('不明')){
             return;
         }
 
-        if(!data[prefecture]){
-            data[prefecture] = {};
-        }
         const year = items[2];
         const month = items[3];
         const date = items[4];
         const d = `${year}/${month}/${date}`;
-        if(!data[prefecture][d]){
-            data[prefecture][d] = 0;
+
+        if(!data[d]){
+            data[d] = {};
         }
-        data[prefecture][d] += 1;
+        if(!data[d][prefecture]){
+            data[d][prefecture] = 0;
+        }
+        data[d][prefecture] += 1;
     });
     await fs.writeFile(join(__dirname, 'parsed_3_24.json'), JSON.stringify(data, null, '\t'));
 }
